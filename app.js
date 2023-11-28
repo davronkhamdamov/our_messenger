c589113b2679d9cdaac6dd472528e239b833a7b7
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2023-11-28 19:45:35 +0500

    added cors config

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const { Server } = require("socket.io");
 const { PORT } = require("./lib/envariment");
+const cors = require("cors")
 const io = new Server(PORT, {});
+
+io.use(cors.config())
 
 io.on("connection", (socket) => {
   socket.emit("hello", "world");
